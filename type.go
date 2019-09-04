ca5db168cd013265adb0cc4b16dc2280269549a9
Alexandre Jacquin <alexandre.jacquin@kisio.com>
2019-09-04 10:39:10 +0200

    Update type.go
    Co-Authored-By: Pierre-Etienne Bougué <pierre-etienne.bougue@kisio.com>

--- type.go
+++ type.go
@@ -31,7 +31,7 @@ func (d DirectionType) MarshalJSON() ([]byte, error) {
 	return buffer.Bytes(), nil
 }
 
-// UnmarshalJSON unmashals a quoted json string to the enum value
+// UnmarshalJSON unmarshals a quoted json string to the enum value
 func (d *DirectionType) UnmarshalJSON(b []byte) error {
 	var j string
 	err := json.Unmarshal(b, &j)

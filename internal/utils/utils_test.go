001ffe2e2e1ccfc5e8c4992d6d6ab98cbc825dae
Benoit Brisset <benoit.brisset@gmail.com>
2021-03-22 21:42:20 +0100

    fmt

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -12,36 +12,36 @@ func TestStringToInt(t *testing.T) {
 
 	assert := assert.New(t)
 
-    result := StringToInt("10", 0)
-    assert.Equal(result, 10)
+	result := StringToInt("10", 0)
+	assert.Equal(result, 10)
 
-    result = StringToInt("aas", 0)
-    assert.Equal(result, 0)
+	result = StringToInt("aas", 0)
+	assert.Equal(result, 0)
 }
 
 func TestAddDateAndTime(t *testing.T) {
 
-    assert := assert.New(t)
+	assert := assert.New(t)
 	require := require.New(t)
 
-    str := "2014-11-12T00:00:00.000Z"
-    date, err := time.Parse(time.RFC3339, str)
+	str := "2014-11-12T00:00:00.000Z"
+	date, err := time.Parse(time.RFC3339, str)
 	require.Nil(err)
 
-    offsetStr := "0001-01-01T00:00:00.000Z"
-    offsetDate, err := time.Parse(time.RFC3339, offsetStr)
+	offsetStr := "0001-01-01T00:00:00.000Z"
+	offsetDate, err := time.Parse(time.RFC3339, offsetStr)
 	require.Nil(err)
 
-    result := AddDateAndTime(date, offsetDate)
-    assert.Equal(result.String(), "2015-11-12 00:00:00 +0000 UTC")
+	result := AddDateAndTime(date, offsetDate)
+	assert.Equal(result.String(), "2015-11-12 00:00:00 +0000 UTC")
 }
 
 func TestCalculateOccupancy(t *testing.T) {
 
 	assert := assert.New(t)
 
-    result := CalculateOccupancy(0)
-    assert.Equal(result, 0)
-    result = CalculateOccupancy(50)
-    assert.Equal(result, 50)
+	result := CalculateOccupancy(0)
+	assert.Equal(result, 0)
+	result = CalculateOccupancy(50)
+	assert.Equal(result, 50)
 }

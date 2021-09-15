fbdd72056228c55d31df1aa87c6df8e2f0dd04d7
julyto <julien.guillemineau@gmail.com>
2021-09-15 15:45:42 +0200

    changed Netex code in Oditi

--- internal/vehicleoccupancies/navitia.go
+++ internal/vehicleoccupancies/navitia.go
@@ -134,7 +134,7 @@ func LoadRoutesWithDirection(startIndex int, uri url.URL, token, direction strin
 	connectionTimeout time.Duration, location *time.Location) ([]RouteSchedule, error) {
 	// Call and load routes for line=40
 	dateTime := time.Now().Truncate(24 * time.Hour).Format("20060102T000000")
-	callUrl := fmt.Sprintf(URL_GET_ROUTES, uri.String(), "line:0:004004040:40", direction, dateTime)
+	callUrl := fmt.Sprintf(URL_GET_ROUTES, uri.String(), "IDFM:C00048:40", direction, dateTime) // old code "line:0:004004040:40"
 	resp, err := CallNavitia(callUrl, token, connectionTimeout)
 	if err != nil {
 		VehicleOccupanciesLoadingErrors.Inc()
@@ -150,13 +150,13 @@ func LoadRoutesWithDirection(startIndex int, uri url.URL, token, direction strin
 	}
 
 	sens := 0
-	if direction == "backward" {
+	if direction == "backward" || direction == "outbound" {
 		sens = 1
 	}
 	routeSchedules := LoadRouteSchedulesData(startIndex, navitiaRoutes, sens, location)
 
 	// Call and load routes for line=45
-	callUrl = fmt.Sprintf(URL_GET_ROUTES, uri.String(), "line:0:004004029:45", direction, dateTime)
+	callUrl = fmt.Sprintf(URL_GET_ROUTES, uri.String(), "IDFM:C00051:45", direction, dateTime) // old code "line:0:004004029:45"
 	resp, err = CallNavitia(callUrl, token, connectionTimeout)
 	if err != nil {
 		VehicleOccupanciesLoadingErrors.Inc()

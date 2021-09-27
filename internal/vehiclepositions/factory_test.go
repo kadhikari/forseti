a85203e9d50f14b399102f69b22bbd6652dae683
julyto <julien.guillemineau@gmail.com>
2021-09-27 10:05:48 +0200

    rename function and variable

--- internal/vehiclepositions/factory_test.go
+++ internal/vehiclepositions/factory_test.go
@@ -7,20 +7,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_VehicleLocationFactory(t *testing.T) {
+func Test_VehiclePositionFactory(t *testing.T) {
 	require := require.New(t)
 	assert := assert.New(t)
 
 	// Create vehicle locations type of GTFS-RT
-	vehiclelocations, err := ConnectorFactory("gtfsrt")
+	vehiclepositions, err := ConnectorFactory("gtfsrt")
 	require.Nil(err)
-	_, ok := vehiclelocations.(*GtfsRtContext)
+	_, ok := vehiclepositions.(*GtfsRtContext)
 	require.True(ok)
 
 	// Create vehicle locations type of unknown
-	vehiclelocations, err = ConnectorFactory("unknown")
+	vehiclepositions, err = ConnectorFactory("unknown")
 	require.NotNil(err)
 	assert.EqualError(err, "Wrong connector type passed")
-	_, ok = vehiclelocations.(*GtfsRtContext)
+	_, ok = vehiclepositions.(*GtfsRtContext)
 	require.False(ok)
 }
